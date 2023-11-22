1fda65f1972e6a486146ef8a09bb5520c7ec9703
Subroto Nag Pinku <egk204@soar-3.usask.ca>
2023-11-22 13:52:34 -0600

    working-linux-gpu

--- setup.sh
+++ setup.sh
@@ -1,16 +1,16 @@
 # python3 -m venv venv
 # source venv/bin/activate
-#use CUDA version if you have GPU
-# pip3 install torch==1.13.0  torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu116
-# pip3 install pyg-lib torch-scatter torch-sparse torch-cluster torch-spline-conv torch-geometric -f https://data.pyg.org/whl/torch-1.13.0+cu116.html
+# use CUDA version if you have GPU
+pip3 install torch==1.13.0  torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu116
+pip3 install pyg-lib torch-scatter torch-sparse torch-cluster torch-spline-conv torch-geometric -f https://data.pyg.org/whl/torch-1.13.0+cu116.html
 #--extra-index-url https://download.pytorch.org/whl/cpu #for cpu
-pip install torch==1.13.0+cpu torchvision==0.14.0+cpu torchaudio==0.13.0 --extra-index-url https://download.pytorch.org/whl/cpu
-pip install torch-scatter torch-sparse torch-cluster torch-spline-conv torch-geometric -f https://data.pyg.org/whl/torch-1.13.0+cpu.html
+# pip install torch==1.13.0+cpu torchvision==0.14.0+cpu torchaudio==0.13.0 --extra-index-url https://download.pytorch.org/whl/cpu
+# pip install torch-scatter torch-sparse torch-cluster torch-spline-conv torch-geometric -f https://data.pyg.org/whl/torch-1.13.0+cpu.html
 
 cd storage
-# wget https://dl.fbaipublicfiles.com/transcoder/pre_trained_models/translator_transcoder_size_from_DOBF.pth -P pretrained/
-# wget https://dl.fbaipublicfiles.com/transcoder/pre_trained_models/TransCoder_model_1.pth -P pretrained/
-# wget https://dl.fbaipublicfiles.com/transcoder/pre_trained_models/TransCoder_model_2.pth -P pretrained/
+wget https://dl.fbaipublicfiles.com/transcoder/pre_trained_models/translator_transcoder_size_from_DOBF.pth -P pretrained/
+wget https://dl.fbaipublicfiles.com/transcoder/pre_trained_models/TransCoder_model_1.pth -P pretrained/
+wget https://dl.fbaipublicfiles.com/transcoder/pre_trained_models/TransCoder_model_2.pth -P pretrained/
 cd ..
 cd codegen_sources/model/tools
 git clone https://github.com/glample/fastBPE.git
@@ -57,3 +57,5 @@ wget http://131.123.42.38/lmcrs/v1.0.0/srcml_1.0.0-1_ubuntu20.04.deb
 sudo dpkg -i srcml_1.0.0-1_ubuntu20.04.deb
 wget http://131.123.42.38/lmcrs/v1.0.0/srcml-dev_1.0.0-1_ubuntu20.04.deb
 sudo dpkg -i srcml-dev_1.0.0-1_ubuntu20.04.deb
+
+pip install pylibsrcml
